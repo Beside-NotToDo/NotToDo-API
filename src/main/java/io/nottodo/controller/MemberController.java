917075a0126985dc1756a134fa30fa18751bf6c0
PeachCoolpis <rlglxh@naver.com>
2024-06-03 21:23:43 +0900

    fix. HttpStatus.OK 로 수정

--- src/main/java/io/nottodo/controller/MemberController.java
+++ src/main/java/io/nottodo/controller/MemberController.java
@@ -5,15 +5,18 @@ import io.nottodo.entity.Member;
 import io.nottodo.jwt.JwtUtil;
 import io.nottodo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
+@ResponseStatus(HttpStatus.OK)
 public class MemberController {
     
     private final MemberService memberService;
